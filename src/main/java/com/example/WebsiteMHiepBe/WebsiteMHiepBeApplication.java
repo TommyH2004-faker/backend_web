a7da5ec0cf6efeba1be02792590964ef4614788a
Tommy <hiept81331@gmail.com>
2025-05-28 08:19:33 +0700

    Đẩy source code từ D:/WebsiteMHiepBe lên GitHub

---
+++ src/main/java/com/example/WebsiteMHiepBe/WebsiteMHiepBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteMHiepBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteMHiepBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteMHiepBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebsiteMHiepBe/WebsiteMHiepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebsiteMHiepBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteMHiepBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
