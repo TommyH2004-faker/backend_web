5f62434238a14de6449a7830a7f26c855879e04e
Tommy <hiept81331@gmail.com>
2025-05-28 09:06:13 +0700

    tạo lop mới cho dự án be

--- src/main/java/com/example/WebsiteMHiepBe/entity/LopHhoc.java
+++ src/main/java/com/example/WebsiteMHiepBe/entity/LopHhoc.java
@@ -1,4 +1,13 @@
 package com.example.WebsiteMHiepBe.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class LopHhoc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String maLop;
 }
