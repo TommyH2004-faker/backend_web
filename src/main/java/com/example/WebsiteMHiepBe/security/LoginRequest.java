75d457a868eedc7e40ac904f70c84cb6c0b6c491
Tommy <hiept81331@gmail.com>
2025-06-02 16:10:24 +0700

    update endpoint va cau hinh thu vien

--- src/main/java/com/example/WebsiteMHiepBe/security/LoginRequest.java
+++ src/main/java/com/example/WebsiteMHiepBe/security/LoginRequest.java
@@ -5,13 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
-
-
 
 
 
